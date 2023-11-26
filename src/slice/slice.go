02927014dda4e3e5751b2b82cdd25c027b3d60e4
sithum-ravishka <sithumravishka1@gmail.com>
2023-11-26 16:03:36 +0530

    update slice

--- src/slice/slice.go
+++ src/slice/slice.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	matrix := [][]int{
@@ -12,4 +15,13 @@ func main() {
 	matrix[0][2] = 20
 
 	fmt.Println(matrix[1][0] * matrix[2][0])
+
+	// a := [5]int{1, 2, 3, 4, 5} //40
+	// b := []int{1, 2, 3, 4, 5}  //24
+
+	k := [5]string{"a", "a", "a", "a", "a"} //80
+	j := []string{"a", "a", "a", "a", "a"}  //24
+
+	fmt.Printf("Array, Size bytes %d \n", unsafe.Sizeof(k))
+	fmt.Printf("Slice, Size bytes %d \n", unsafe.Sizeof(j))
 }
